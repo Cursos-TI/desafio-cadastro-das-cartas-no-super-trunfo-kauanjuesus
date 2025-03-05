073df3157bd629235d90c2af52df2ee085a7ded8
kauanjuesus <kauanjeususpokemon@gmail.com>
2025-03-05 20:15:34 -0300

    Update CartasSuperTrunfo.c
    basicamente eu coloquei o pib per capital  e a densidade populacional alem de tentar deixa as respostas mais legivel para quem estiver executando.
    p.s tenho muito quer melhora xd

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,8 +1,8 @@
-#include <stdio.h>
+	#include <stdio.h>
 
 int main(){
 	int pop,ndpt,pop2,ndpt2;
-	float area,pib,area2,pib2;
+	float area,pib,area2,pib2,densidade,densidade2,pibc,pibc2;
 	printf("DADOS DA PRIMEIRA CARTA \n");
 	printf("qual a populacao ? ");
 	scanf("%d", &pop);
@@ -12,6 +12,8 @@ int main(){
 	scanf("%f", &pib);
 	printf("quantos numeros turisticos tem? ");
 	scanf("%d", &ndpt);
+	densidade= pop/area;
+	pibc= pib/pop;
 	printf("DADOS DA SEGUNDA CARTA\n");
 	printf("qual a populacao ? ");
 	scanf("%d", &pop2);
@@ -21,15 +23,20 @@ int main(){
 	scanf("%f", &pib2);
 	printf("quantos numeros turisticos tem? ");
 	scanf("%d", &ndpt2);
+	densidade2= pop2/area2;
+	pibc2= pib2/pop2;
 	printf("TODOS OS DADOS REGISTRADOS DA PRIMEIRA CARTA \n");
-	printf("populacao:  %d\n", pop, "");
-	printf("area: %f\n", area, "");
-	printf("pib: %f\n", pib, "");
+	printf("populacao:  %d de habitantes\n", pop, "");
+	printf("area: %2.f de metros quadrados\n", area, "");
+	printf("pib: %2.f\n", pib, "");
 	printf("numeros de pontos turisticos: %d\n", ndpt);
+	printf("densidade populacional: %2.f\n", densidade);
+	printf("pib per capital: %2.f\n", pibc);
 	printf("TODOS OS DADOS REGISTRADOS DA SEGUNDA CARTA \n");
-	printf("populacao:  %d\n", pop2, "");
-	printf("area: %f\n", area2, "");
-	printf("pib: %f\n", pib2, "");
+	printf("populacao:  %d de habitantes\n", pop2, "");
+	printf("area: %2.f de metros quadrados\n", area2, "");
+	printf("pib: %2.f\n", pib2, "");
 	printf("numeros de pontos turisticos: %d\n", ndpt2);
+	printf("densidade populacional: %2.f\n", densidade2);
+	printf("pib per capital: %2.f\n", pibc2);
 	return 0;
-}
